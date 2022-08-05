7ef5685e792247e7d1f0ef0d0bfba22364d4ef37
dldvlpr <dormoy.dev@gmail.com>
2022-08-05 12:01:58 +0200

    fix login logout and fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,9 +2,9 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
 use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -19,23 +19,20 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create();
+        $faker = Factory::create('fr_FR');
+        $user = new User();
+        $user->setEmail($faker->email())
+            ->setFirstname($faker->firstName())
+            ->setLastname($faker->lastName())
+            ->setPhonenumber('0512567890');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            'password'
+        );
+        $user->setPassword($hashedPassword);
 
-        for ($i = 0; $i < 5; $i++) {
+        $manager->persist($user);
 
-            $user = new User();
-            $hashedPassword = $this->passwordHasher->hashPassword(
-                $user,
-                'password'
-            );
-            $user->setEmail($faker->email())
-                ->setFirstname($faker->firstName())
-                ->setLastname($faker->lastName())
-                ->setPhonenumber(1234567890)
-                ->setPassword($hashedPassword);
-
-            $manager->persist($user);
-        }
         $manager->flush();
     }
 }
