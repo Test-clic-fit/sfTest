6b466b853adfa380f7bb91c8fe61735fe8fd1fcc
dldvlpr <dormoy.dev@gmail.com>
2022-08-06 15:11:10 +0200

    item form ok

--- src/Form/CraiglistFormType.php
+++ src/Form/CraiglistFormType.php
@@ -4,21 +4,37 @@ namespace App\Form;
 
 use App\Entity\Craiglist;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class CraiglistFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('picture')
-            ->add('zipcode')
-            ->add('date')
-            ->add('vendor')
-        ;
+            ->add('title', TextType::class)
+            ->add('description', TextType::class)
+            ->add('picture', FileType::class, [
+                'label' => 'Image',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '20000k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],
+            ])
+            ->add('zipcode', IntegerType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
